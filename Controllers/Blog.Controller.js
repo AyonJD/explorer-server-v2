1cccbb018c0d94aaa98d5db579594778d22c7c92
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-02-02 19:08:38 +0600

    Blog by id

--- Controllers/Blog.Controller.js
+++ Controllers/Blog.Controller.js
@@ -8,3 +8,29 @@ module.exports.blogCount = async (req, res) => {
         res.status(500).json({ success: false, message: "Internal server error", error: err.message });
     }
 };
+
+module.exports.getAllBlogWithPegination = async (req, res) => {
+    const page = parseInt(req.query.page);
+    const count = parseInt(req.query.count);
+    try {
+        let blogs;
+        if (page || count) {
+            blogs = await BlogModel.find({}).skip(page * count).limit(count);
+        } else {
+            blogs = await BlogModel.find({});
+        }
+        res.send(blogs);
+    } catch (err) {
+        res.status(500).json({ success: false, message: "Internal server error", error: err.message });
+    }
+};
+
+module.exports.getBlogById = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const blog = await BlogModel.findById(id);
+        res.send(blog);
+    } catch (err) {
+        res.status(500).json({ success: false, message: "Internal server error", error: err.message });
+    }
+};
--- Routes/Blog.Route.js
+++ Routes/Blog.Route.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express');
+const { blogCount, getAllBlogWithPegination } = require('../Controllers/Blog.Controller');
 const router = express.Router();
 
-router.get('/blogs-count', (req, res) => { });
+router.get('/blogs-count', blogCount);
+router.get('/', getAllBlogWithPegination);
 
 module.exports = router;
