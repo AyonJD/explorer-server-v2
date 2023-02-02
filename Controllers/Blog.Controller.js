2db1baa1266471151f4dcfb452682aff1d834ca8
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-02-02 19:02:51 +0600

    Blog count for pegination

---
+++ Controllers/Blog.Controller.js
@@ -0,0 +1,11 @@
+const BlogModel = require('../Models/Blog.Model.js');
+
+module.exports.blogCount = async (req, res) => {
+    try {
+        const blogs = await BlogModel.find();
+        const blogsCount = await BlogModel.countDocuments({});
+        res.status(200).json({ success: true, message: "Successfully fetched all blogs", blogsCount });
+    } catch (err) {
+        res.status(500).json({ success: false, message: "Internal server error", error: err.message });
+    }
+};
