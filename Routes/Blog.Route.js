79fa5bc2ce4e859b43088d6e4fb1b2307069cf62
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-02-02 19:13:05 +0600

    Blog done

--- Routes/Blog.Route.js
+++ Routes/Blog.Route.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { blogCount, getAllBlogWithPegination } = require('../Controllers/Blog.Controller');
+const { blogCount, getAllBlogWithPegination, getBlogById, updateBlog } = require('../Controllers/Blog.Controller');
 const router = express.Router();
 
 router.get('/blogs-count', blogCount);
 router.get('/', getAllBlogWithPegination);
+router.get('/:id', getBlogById);
+router.post('/', createBlog);
+router.put('/:id', updateBlog);
 
 module.exports = router;
