40d66e30964247185674ac055c4ee432a6eb5a6a
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-02-02 18:10:45 +0600

    User route connect with controller

--- Routes/User.Route.js
+++ Routes/User.Route.js
@@ -1,10 +1,11 @@
-const express = require('express')
+const express = require('express');
+const { createUser, getUser, loginUser, getSingleUser, updateUser } = require('../Controllers/User.Controller');
 const router = express.Router();
 
-router.get('/', (req, res) => { });
-router.post('/signup', (req, res) => { })
-router.post('/login', (req, res) => { })
-router.get('/:id', (req, res) => { })
-router.patch('/:id', (req, res) => { })
+router.get('/', getUser);
+router.post('/signup', createUser)
+router.post('/login', loginUser)
+router.get('/:id', getSingleUser)
+router.patch('/:email', updateUser)
 
 module.exports = router;
